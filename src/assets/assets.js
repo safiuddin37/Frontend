dccbd80caadb3c619b05502964ddbdfcfafeb969
stunning-star <mohdsafiuddin99@gmail.com>
2025-05-10 12:46:15 +0530

    changes

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,5 @@
 import bg_books from './bg_books_image.jpg'
-import boys from './Muslim_boys_studying.JPg'
+import boys from './Muslim_boys_studying.jpg'
 import about_image from './About_img.jpg'
 import Programs_image from './program_img.jpg'
 import transformationStoryOne from './firstOne.png'
