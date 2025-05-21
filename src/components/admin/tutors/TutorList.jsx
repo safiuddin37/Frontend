e17f2d3d25aa1c2e582b69668fe3f0361977cdf2
Md Naseer uddin <umdnaseer17@gmail.com>
2025-05-21 18:37:07 +0530

    tutor status

--- src/components/admin/tutors/TutorList.jsx
+++ src/components/admin/tutors/TutorList.jsx
@@ -340,7 +340,7 @@ const TutorList = ({ onEdit, onDelete, onProfile }) => {
                       </button>
                     </div>
                   </td>
-                  <td style={{ padding: '14px 16px', color: '#4b5563', backgroundColor:'green' }}>{tutor.status}</td>
+                  <td style={{ padding: '14px 16px', color: '#4b5563', color:'green' }}>{tutor.status}</td>
                 </tr>
               ))
             )}
