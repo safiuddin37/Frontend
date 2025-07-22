b24ac2b17669b361bdcfe21cc62b5a3b89625d1e
codingsenpy <mustafakunain06@gmail.com>
2025-07-22 12:49:17 +0530

    character field len

--- .history/src/components/supervisor/SupervisorDashboard_20250608194810.jsx
+++
@@ -1,7 +0,0 @@
-import { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import Overview from './Overview';
-import TutorManagement from './TutorManagement';
-
-const admin
